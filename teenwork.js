6fa9841a2d8686a81c69fe16363881b51ca1b20a
Sammiex0x <Sammiex0x@hotmail.com>
2017-03-29 01:34:10 -0700

    added coursel, map table etc

--- teenwork.js
+++ teenwork.js
@@ -10,3 +10,60 @@ $(".item").click(function(){
 $(".left").click(function(){
     $("#myCarousel").carousel("prev");
 });
+
+var locations = [
+   [
+        "Teenwork @ CARSA Building",
+        48.467922,
+        -123.310904
+       
+    ],
+    [
+    	"Parking Lot 12",
+        48.467394,
+        -123.308372
+ 
+    ],    
+    [
+        "Bike Parking",
+        48.464948,
+         -123.312175
+         
+    ],
+]
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: new google.maps.LatLng(48.465805, -123.310930),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3]),
+        map: map
+      });
+
+    var count = 0;
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+           if (locations[i][1]) {
+            alert("Latitude,Longitude:  48.467922,-123.310904");
+       
+        } else if (locations[i][2]) {
+            alert("Latitude,Longitude:  48.468146, -123.308372" + count);
+         
+        } else {
+            alert("Latitude,Longitude:  48.464948, -123.312175" + count)
+          
+        };
+        }
+      })(marker, i));
+    }
